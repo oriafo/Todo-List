eba601407034a650da5ba443bd78dcfd420c678b
oriafo <abrahamibharunujele@gmail.com>
2020-08-04 23:59:16 -0700

    first phase

--- asset/js/Todo.js
+++ asset/js/Todo.js
@@ -14,8 +14,14 @@ $("ul").on("click", "span", function(event){
 $("input[type='text']").keypress(function(event){
     if(event.which === 13){
         var todoText = $(this).val();
-        $(this).val("");
-        //create a new li and add it to ul
-        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")        
+        if(todoText.length < 30){
+            $(this).val("");
+            //create a new li and add it to ul
+            $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
+        }
+        else{
+            $(this).val("");
+        }
     }
 });
+
